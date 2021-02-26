8eb5958dd3c42224bf77486a93847018475a9952
Mladen Nikolov <mladen.nikolov@convesys.com>
2021-02-26 11:44:03 +0200

    [IMPL] Added run_all.bat.

---
+++ run_all.bat
@@ -0,0 +1,4 @@
+x64\Release\CommTest.exe sender_port=COM3 receiver_port=COM6 api=d2xx notification_mask=1 receiver_log= > d2xx_mask_1_receiver_232H.txt
+x64\Release\CommTest.exe sender_port=COM6 receiver_port=COM3 api=d2xx notification_mask=1 receiver_log= > d2xx_mask_1_receiver_232R.txt
+x64\Release\CommTest.exe sender_port=COM3 receiver_port=COM6 api=d2xx notification_mask=7 receiver_log= > d2xx_mask_7_receiver_232H.txt
+x64\Release\CommTest.exe sender_port=COM3 receiver_port=COM6 api=vcp receiver_log= > vcp_receiver_232H.txt
